f4c687a1376902d50828501d0410607c288db61c
RoflanRabotyaga <andrey0kulinich@gmail.com>
2024-08-13 19:26:10 +0300

    php2 Lab1.3 + navigation menu

--- php2/php2_labs_den1_content/q3.php
+++ php2/php2_labs_den1_content/q3.php
@@ -1,4 +1,4 @@
-<form id="submit" method="post" style="position: absolute; width: 200px; height: 200px; overflow: auto; margin: auto; top: 0; left: 0; bottom: 0; right: 0">
+<form id="submit" method="post">
     <input type='hidden' name='q' value='<?= ++$q ?>'>
     <input type='hidden' name='title' value='x plus 99 is 9 times less than 999. What is 9 less than x ?'>
     <input type="radio" name="answer" id="3a" value="3a"><label for="3a">a. x = 33 </label><br/><br/>
